0fa690a9f7e40358e4cb5652d49a15df70dc24c9
Joshua Kleiner <jkleiner@MIAGRBISSH01V.stg.rbi.local>
2013-10-23 14:57:48 -0400

    first commit

---
+++ portcheck.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import socket
+import sqlite
+import sys
+
+database = "/rbiops/share/portcheck.db"
+#For future reference: CREATE TABLE endpoints ( description text, address text, port integer, PRIMARY KEY (description) );
+
+def portcheck(description,address,port):
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	result = s.connect_ex(( address , int(port)))
+
+	if(result == 0) :
+    		print '%s socket is open: Host %s Port %s' % (description, address, port)
+	else :
+		print '%s socket is closed: Host %s Port %s' % (description, address, port)
+	s.close()
+
+try:
+	conn = sqlite.connect(database)
+except:
+	print "Could not find database file %s, or incorrect permissions" % database
+	exit()
+
+cur = conn.cursor()
+cur.execute('SELECT * FROM endpoints')
+
+for row in cur.fetchall():
+	portcheck(row[0],row[1],row[2]);
+
+conn.close()
