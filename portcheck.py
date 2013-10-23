3de0c20a9e757b33d85b2c7699e7c3653a3bcc96
Joshua Kleiner <joshewah@gmail.com>
2013-10-23 18:17:28 -0400

    Added color variables

--- portcheck.py
+++ portcheck.py
@@ -6,14 +6,17 @@ import sys
 database = "/rbiops/share/portcheck.db"
 #For future reference: CREATE TABLE endpoints ( description text, address text, port integer, PRIMARY KEY (description) );
 
+OPEN = '\33[94m'
+CLOSED = '\33[31m'
+
 def portcheck(description,address,port):
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	result = s.connect_ex(( address , int(port)))
 
 	if(result == 0) :
-		print '\33[94m%s socket is open: Host %s Port %s\033[0m' % (description, address, port)
+		print OPEN + '%s socket is open: Host %s Port %s\033[0m' % (description, address, port)
 	else :
-		print '\33[31m%s socket is closed: Host %s Port %s\033[0m' % (description, address, port)
+		print CLOSE + '%s socket is closed: Host %s Port %s\033[0m' % (description, address, port)
 	s.close()
 
 try:
