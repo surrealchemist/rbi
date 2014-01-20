2d39f82d80e70764135680f9da5aefbd8af990c7
Joshua Kleiner <joshewah@gmail.com>
2014-01-20 13:15:45 -0500

    Adding an option for the port

--- solrindexcheck.py
+++ solrindexcheck.py
@@ -9,9 +9,9 @@ try:
 except ImportError:
     import simplejson as json
 
-def getIndexInfo (solrAddress,index):
+def getIndexInfo (solrAddress,port,index):
 
-	URL = ("http://") + solrAddress + (":8983/solr/") + index + ("/replication?command=details&wt=json")
+	URL = ("http://") + solrAddress + (":") + port + ("/solr/") + index + ("/replication?command=details&wt=json")
 	try:
 	        Response = urllib2.urlopen(URL)
                 stringResponse = Response.read()
@@ -29,13 +29,13 @@ def getIndexInfo (solrAddress,index):
  
 
 if len(sys.argv) == 1:
-	print "Please provide a server URL or IP, and the index you wish to check"
+	print "Please provide a server URL or IP, port,  and the index you wish to check"
 	print "Example:"
 	print "solrindexcheck.py 192.168.1.1 myindex1"
-elif len(sys.argv) > 3:
-	print "Please provide only 2 arguments (URL/IP and the index to check)"
+elif len(sys.argv) > 4:
+	print "Please provide only 3 arguments (URL/IP and the index to check)"
 	print "Example:"
-	print "solrindexcheck.py 192.168.1.1 myindex1"
+	print "solrindexcheck.py 192.168.1.1 8983 myindex1"
 else:
-	getIndexInfo(sys.argv[1],sys.argv[2])
+	getIndexInfo(sys.argv[1],sys.argv[2],sys.argv[3])
 	
